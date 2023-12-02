0e8dd15920ff50a7457945090cb1425283551c1a
Jacob Thrasher <jthrasher0100@gmail.com>
2023-12-02 17:41:19 -0500

    VAE train/test split

--- sandbox.py
+++ sandbox.py
@@ -3,23 +3,45 @@ import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 from network import CNNClassifier, VAE
 import matplotlib.pyplot as plt
+import pandas as pd
+import csv
+from tqdm import tqdm
 
-# This is a change
+df = pd.read_csv('data\\test_data.csv')  
+train_subjects = list(df['Image Name'])
 
-print(os.getcwd())
-train_root = '/home/jacob/Documents/data/archive/SOCOFing/Altered/Altered-Hard'
-gt_root = '/home/jacob/Documents/data/archive/SOCOFing/Real'
-train_samples, test_samples = socofing_train_test_split_gen(train_root, test_size=32)
+diff = ['Easy', 'Medium', 'Hard']
+root = 'D:\\Big_Data\\SOCOFing\\SOCOFing\\Altered'
 
-print(len(train_samples), len(test_samples))
+f = open('data\\vae_test.csv', 'w', newline='')
+writer = csv.writer(f)
+writer.writerow(["Image Name", "Number", "Gender", "Hand", "Finger", "Alteration", "Difficulty"])
 
-train_dataset = SOCOFing_Gen(train_root, gt_root, train_samples, test_samples)
+for d in diff:
+    data_path = os.path.join(root, f'Altered-{d}')
+    for filename in tqdm(os.listdir(data_path)):
+        attr = filename.split('_')
+        gt_filename = attr[0] + '__' + attr[2] + '_' + attr[3] + '_' + attr[4] + '_finger.BMP'
 
-train, gt = train_dataset[0]
-fig, (ax1, ax2) = plt.subplots(1, 2)
-ax1.imshow(train, cmap='gray')
-ax2.imshow(gt, cmap='gray')
-fig.savefig('test.png')
+        if gt_filename in train_subjects:
+            writer.writerow([filename, attr[0], attr[2], attr[3], attr[4], attr[6].split('.')[0], d])
+
+f.close()
+
+# print(os.getcwd())
+# train_root = '/home/jacob/Documents/data/archive/SOCOFing/Altered/Altered-Hard'
+# gt_root = '/home/jacob/Documents/data/archive/SOCOFing/Real'
+# train_samples, test_samples = socofing_train_test_split_gen(train_root, test_size=32)
+
+# print(len(train_samples), len(test_samples))
+
+# train_dataset = SOCOFing_Gen(train_root, gt_root, train_samples, test_samples)
+
+# train, gt = train_dataset[0]
+# fig, (ax1, ax2) = plt.subplots(1, 2)
+# ax1.imshow(train, cmap='gray')
+# ax2.imshow(gt, cmap='gray')
+# fig.savefig('test.png')
 
 # dataloader = DataLoader(dataset, batch_size=32)
 # batch = next(iter(dataloader))
